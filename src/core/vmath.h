a04b5dcf86c09ecc2d804e0e78c2ae1fd6199cc2
Jamorn Sriwasansak <jamorn.sriwasansak@gmail.com>
2021-09-26 17:21:02 +0900

    add half type

--- src/core/vmath.h
+++ src/core/vmath.h
@@ -2,6 +2,7 @@
 
 #pragma warning(push, 0)
 #include <glm/glm.hpp>
+#include <glm/gtc/packing.hpp>
 #include <glm/gtx/compatibility.hpp>
 #include <glm/gtx/string_cast.hpp>
 #pragma warning(pop)
@@ -12,10 +13,51 @@ using namespace glm;
 using uint2 = glm::uvec2;
 using uint3 = glm::uvec3;
 using uint4 = glm::uvec4;
-using half  = glm::u16;
-using half2 = glm::u16vec2;
-using half3 = glm::u16vec3;
-using half4 = glm::u16vec4;
+
+template <uint8_t NumElements, glm::qualifier Qualifier = glm::qualifier::defaultp>
+struct halfN
+{
+    glm::vec<NumElements, uint16_t, Qualifier> val;
+
+    halfN() {}
+
+    halfN(const glm::vec<NumElements, float, Qualifier> & v)
+    {
+        static_assert(sizeof(uint16_t) == sizeof(short),
+                      "To use glm::toFloat16, sizeof uint16_t must strictly equals to short");
+
+        if constexpr (NumElements == 1)
+        {
+            val[0] = glm::detail::toFloat16(v[0]);
+        }
+        else if constexpr (NumElements == 2)
+        {
+            val[0] = glm::detail::toFloat16(v[0]);
+            val[1] = glm::detail::toFloat16(v[1]);
+        }
+        else if constexpr (NumElements == 3)
+        {
+            val[0] = glm::detail::toFloat16(v[0]);
+            val[1] = glm::detail::toFloat16(v[1]);
+            val[2] = glm::detail::toFloat16(v[2]);
+        }
+        else if constexpr (NumElements == 4)
+        {
+            val[0] = glm::detail::toFloat16(v[0]);
+            val[1] = glm::detail::toFloat16(v[1]);
+            val[2] = glm::detail::toFloat16(v[2]);
+            val[3] = glm::detail::toFloat16(v[3]);
+        }
+        else
+        {
+            static_assert(false, "halfN only support 4 >= NumElements >= 0");
+        }
+    }
+};
+using half  = halfN<1>;
+using half2 = halfN<2>;
+using half3 = halfN<3>;
+using half4 = halfN<4>;
 
 struct urange
 {
