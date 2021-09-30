8d17da1b97a08c3279d5cbe695f10c6be5ffddd7
Jamorn Sriwasansak <jamorn.sriwasansak@gmail.com>
2021-10-01 05:24:58 +0900

    type.h

---
+++ src/shaders/shared/type.h
@@ -0,0 +1,10 @@
+#ifndef TYPE_H
+#define TYPE_H
+typedef uint32_t TextureId;
+typedef uint32_t MaterialId;
+typedef uint16_t Index;
+typedef float3 Position3;
+typedef float3 Normal3;
+typedef uint32_t VerticesSize;
+typedef uint32_t IndicesSize;
+#endif // TYPE_H
